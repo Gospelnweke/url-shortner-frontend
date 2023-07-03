be2e533bf969ed0e1cb7055605eb5acdcf0362f6
Gospelnweke <48245790+Gospelnweke@users.noreply.github.com>
2023-07-03 13:05:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     console.log(`long url: ${longUr}`);
 
     axios
-      .post('http://gospily-api/index', { longUrl: longUr })
+      .post('http://gospily-api.onrender.com/index', { longUrl: longUr })
       .then((res) => {
         const { shortUrl } = res.data;
         //const realShortUrl = shortUrl.substring(22);
